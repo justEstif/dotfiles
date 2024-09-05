35113b43d6b6d3e46588e86bc3f9cd821a840ed5
e-beyene <ebeyene@redventures.com>
2024-09-05 10:25:40 -0400

    fix: linux abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,6 +1,5 @@
 # abbreviations
 abbr -a v nvim
-abbr -a update "sudo apt update && sudo apt upgrade"
 abbr -a lz lazygit
 abbr -a killnode "killall -9 node" # kill all node apps
 
@@ -9,4 +8,5 @@ set -l os (uname)
 if test "$os" = Linux
   abbr -a dark_theme "gsettings set org.gnome.desktop.interface color-scheme prefer-dark"
   abbr -a light_theme "gsettings set org.gnome.desktop.interface color-scheme prefer-light"
+  abbr -a update "sudo apt update && sudo apt upgrade"
 end
