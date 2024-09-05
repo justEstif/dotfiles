3f96d1dfa29a912ed38dd6e80ebaee6c748bb831
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-09-04 21:22:13 -0400

    fix: linux fzf path

--- fish/.config/fish/conf.d/paths.fish
+++ fish/.config/fish/conf.d/paths.fish
@@ -2,5 +2,10 @@ set -e fish_user_paths
 
 set -U fish_user_paths $HOME/.local/share/nvim/mason/bin $fish_user_paths
 set -U fish_user_paths $HOME/.asdf/shims $fish_user_paths
-set -U fish_user_paths $HOME/.asdf/bin/ $fish_user_paths
-# set -U fish_user_paths $HOME/.fzf/bin $fish_user_paths
+set -U fish_user_paths $HOME/.asdf/bin $fish_user_paths
+
+set -l os (uname)
+
+if test "$os" = Linux
+ set -U fish_user_paths $HOME/.fzf/bin $fish_user_paths
+end
