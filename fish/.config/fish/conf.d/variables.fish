06b60ed5a599e44a52a12e02d28fbfc83aa43592
e-beyene <ebeyene@redventures.com>
2024-09-09 13:51:17 -0400

    feat: ripgreprc config

--- fish/.config/fish/conf.d/variables.fish
+++ fish/.config/fish/conf.d/variables.fish
@@ -2,4 +2,5 @@ set -gx EDITOR nvim
 set fish_greeting
 
 set -x ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
+set -x RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
 set -x XDG_CONFIG_HOME $HOME/.config
