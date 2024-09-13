051038820cb44bc28c182e8340293190f8957f0b
e-beyene <ebeyene@redventures.com>
2024-09-13 12:09:19 -0400

    feat: xplr zoxide 🤝

--- xplr/.config/xplr/init.lua
+++ xplr/.config/xplr/init.lua
@@ -2,8 +2,23 @@ version = "0.21.9"
 local xplr = xplr
 
 xplr.config.modes.builtin.default.key_bindings.on_key.q = {
-  help = "quit and print pwd",
-  messages = {
-     "PrintPwdAndQuit",
-  },
+	help = "quit and print pwd",
+	messages = {
+		"PrintPwdAndQuit",
+	},
+}
+
+xplr.config.modes.builtin["default"].key_bindings.on_key["Z"] = {
+	help = "zoxide jump",
+	messages = {
+		{
+			BashExec = [===[
+          PTH="$(zoxide query -i)"
+          if [ -d "$PTH" ]; then
+            "$XPLR" -m "ChangeDirectory: %q" "${PTH:?}"
+          fi
+        ]===],
+		},
+		"PopMode",
+	},
 }
