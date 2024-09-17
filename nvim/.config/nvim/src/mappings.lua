f330673a90354aac58312be40f936885e579e0ea
e-beyene <ebeyene@redventures.com>
2024-09-17 13:05:56 -0400

    feat: remove term config

--- nvim/.config/nvim/src/mappings.lua
+++ nvim/.config/nvim/src/mappings.lua
@@ -21,10 +21,6 @@ keymap("n", "<space>", "<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.query)
 })
 keymap("v", "p", [["_dP]], { desc = "Keep the yanked text when pasting in visual  mode" })
 
--- terminal
-keymap("n", "<C-t>", ":Term<CR>", { desc = "Open term" })
-keymap("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit term" })
-
 -- use 'cl' instead of 's'
 keymap("n", [[s]], [[<Nop>]])
 keymap("x", [[s]], [[<Nop>]])
