53105f2845a1c8ebb31ec6bd8a728d0e7bb826de
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-09-30 06:49:40 -0400

    feat: update mini files

--- nvim/.config/nvim/src/plugins/mini/files.lua
+++ nvim/.config/nvim/src/plugins/mini/files.lua
@@ -1,106 +1,3 @@
-local function open_in_split(files)
-	local function open_file_in_split(close, vertical)
-		return function()
-			local is_file = files.get_fs_entry().fs_type == "file"
-			if is_file then
-				vim.api.nvim_win_call(files.get_target_window(), function()
-					vim.cmd.split({
-						mods = {
-							split = "belowright",
-							vertical = vertical,
-						},
-					})
-					files.set_target_window(vim.api.nvim_get_current_win())
-				end)
-			end
-
-			files.go_in()
-			if close and is_file then
-				files.close()
-			end
-		end
-	end
-
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "MiniFilesBufferCreate",
-		callback = function(args)
-			local buf_id = args.data.buf_id
-
-			vim.api.nvim_buf_set_keymap(buf_id, "n", "<C-s>", "", {
-				callback = open_file_in_split(true, true),
-				desc = "Open file in vertical split and close file browser",
-			})
-
-			vim.api.nvim_buf_set_keymap(buf_id, "n", "<C-w>s", "", {
-				callback = open_file_in_split(false),
-				desc = "Open file in horizontal split",
-			})
-
-			vim.api.nvim_buf_set_keymap(buf_id, "n", "<C-w>v", "", {
-				callback = open_file_in_split(false, true),
-				desc = "Open file in vertical split",
-			})
-
-			vim.api.nvim_buf_set_keymap(buf_id, "n", "<C-x>", "", {
-				callback = open_file_in_split(true),
-				desc = "Open file in horizontal split and close file browser",
-			})
-		end,
-	})
-end
-
-local function set_cwd(files)
-	local files_set_cwd = function()
-		-- Works only if cursor is on the valid file system entry
-		local cur_entry_path = files.get_fs_entry().path
-		local cur_directory = vim.fs.dirname(cur_entry_path)
-		vim.fn.chdir(cur_directory)
-	end
-
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "MiniFilesBufferCreate",
-		callback = function(args)
-			vim.keymap.set("n", "g~", files_set_cwd, {
-				buffer = args.data.buf_id,
-				desc = "Set current working directory",
-			})
-		end,
-	})
-end
-
-local function toggle_hidden(files)
-	local show_hidden = true
-
-	local filter_show = function()
-		return true
-	end
-
-	local filter_hide = function(fs_entry)
-		return not (vim.startswith(fs_entry.name, ".") or (fs_entry.name == "node_modules"))
-	end
-
-	local hidden_toggler = function()
-		show_hidden = not show_hidden
-		local new_filter = show_hidden and filter_show or filter_hide
-		files.refresh({
-			content = {
-				filter = new_filter,
-			},
-		})
-	end
-
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "MiniFilesBufferCreate",
-		callback = function(args)
-			local buf_id = args.data.buf_id
-			vim.keymap.set("n", "g.", hidden_toggler, {
-				buffer = buf_id,
-				desc = "Toggle hidden files",
-			})
-		end,
-	})
-end
-
 local files = require("mini.files")
 files.setup({
 	mappings = {
@@ -110,6 +7,57 @@ files.setup({
 		go_out_plus = "h",
 	},
 })
-toggle_hidden(files)
-set_cwd(files)
-open_in_split(files)
+
+-- Open in split
+local map_split = function(buf_id, lhs, direction)
+	local rhs = function()
+		-- Make new window and set it as target
+		local cur_target = files.get_explorer_state().target_window
+		local new_target = vim.api.nvim_win_call(cur_target, function()
+			vim.cmd(direction .. " split")
+			return vim.api.nvim_get_current_win()
+		end)
+
+		files.set_target_window(new_target)
+	end
+
+	-- Adding `desc` will result into `show_help` entries
+	local desc = "Split " .. direction
+	vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		-- Tweak keys to your liking
+		map_split(buf_id, "<C-s>", "belowright horizontal")
+		map_split(buf_id, "<C-v>", "belowright vertical")
+	end,
+})
+
+-- Show/hide dot-files
+local show_dotfiles = true
+
+local filter_show = function(fs_entry)
+	return true
+end
+
+local filter_hide = function(fs_entry)
+	return not vim.startswith(fs_entry.name, ".")
+end
+
+local toggle_dotfiles = function()
+	show_dotfiles = not show_dotfiles
+	local new_filter = show_dotfiles and filter_show or filter_hide
+	files.refresh({ content = { filter = new_filter } })
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		-- Tweak left-hand side of mapping to your liking
+		vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+	end,
+})
