310cda314a73edf73afb13d08596a8e12389e11c
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-10-01 05:42:02 -0400

    feat: update files config

--- nvim/.config/nvim/src/plugins/mini/files.lua
+++ nvim/.config/nvim/src/plugins/mini/files.lua
@@ -9,20 +9,25 @@ files.setup({
 })
 
 -- Open in split
-local map_split = function(buf_id, lhs, direction)
+local map_split = function(buf_id, lhs, direction, close_on_file)
 	local rhs = function()
-		-- Make new window and set it as target
-		local cur_target = files.get_explorer_state().target_window
-		local new_target = vim.api.nvim_win_call(cur_target, function()
-			vim.cmd(direction .. " split")
-			return vim.api.nvim_get_current_win()
-		end)
+		local new_target_window
+		local cur_target_window = files.get_explorer_state().target_window
+		if cur_target_window ~= nil then
+			vim.api.nvim_win_call(cur_target_window, function()
+				vim.cmd("belowright " .. direction .. " split")
+				new_target_window = vim.api.nvim_get_current_win()
+			end)
 
-		files.set_target_window(new_target)
+			files.set_target_window(new_target_window)
+			files.go_in({ close_on_file = close_on_file })
+		end
 	end
 
-	-- Adding `desc` will result into `show_help` entries
-	local desc = "Split " .. direction
+	local desc = "Open in " .. direction .. " split"
+	if close_on_file then
+		desc = desc .. " and close"
+	end
 	vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
 end
 
@@ -31,8 +36,8 @@ vim.api.nvim_create_autocmd("User", {
 	callback = function(args)
 		local buf_id = args.data.buf_id
 		-- Tweak keys to your liking
-		map_split(buf_id, "<C-s>", "belowright horizontal")
-		map_split(buf_id, "<C-v>", "belowright vertical")
+		map_split(buf_id, "<C-s>", "horizontal", false)
+		map_split(buf_id, "<C-v>", "vertical", false)
 	end,
 })
 
