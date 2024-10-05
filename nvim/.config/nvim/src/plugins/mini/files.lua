5a7c0a5083113e50ae129daf6350e99547e44712
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-10-05 19:36:28 -0400

    feat: add cwd keybind to mini.files

--- nvim/.config/nvim/src/plugins/mini/files.lua
+++ nvim/.config/nvim/src/plugins/mini/files.lua
@@ -66,3 +66,18 @@ vim.api.nvim_create_autocmd("User", {
 		vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
 	end,
 })
+
+--- Set CWD
+local files_set_cwd = function(path)
+	-- Works only if cursor is on the valid file system entry
+	local cur_entry_path = MiniFiles.get_fs_entry().path
+	local cur_directory = vim.fs.dirname(cur_entry_path)
+	vim.fn.chdir(cur_directory)
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		vim.keymap.set("n", "g~", files_set_cwd, { buffer = args.data.buf_id })
+	end,
+})
