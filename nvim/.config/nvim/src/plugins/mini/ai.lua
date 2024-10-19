6acfdbbdcfabe2219ae4afb97b44a7388609ead2
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-10-19 09:25:44 -0400

    Increase mini.ai number of lines searched

--- nvim/.config/nvim/src/plugins/mini/ai.lua
+++ nvim/.config/nvim/src/plugins/mini/ai.lua
@@ -2,6 +2,7 @@ local ai = require("mini.ai")
 local gen_ai_spec = require("mini.extra").gen_ai_spec
 
 ai.setup({
+  n_lines = 100,
 	custom_textobjects = {
 		F = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
 		B = gen_ai_spec.buffer(),
