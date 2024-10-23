bed10ff2183d031d9335554596b3360251d31692
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-10-23 06:04:07 -0400

    Add goland variable

--- fish/.config/fish/conf.d/variables.fish
+++ fish/.config/fish/conf.d/variables.fish
@@ -2,5 +2,6 @@ set -gx EDITOR nvim
 set fish_greeting
 
 set -x ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
+set -x ASDF_GOLANG_MOD_VERSION_ENABLED true
 set -x RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
 set -x XDG_CONFIG_HOME $HOME/.config
