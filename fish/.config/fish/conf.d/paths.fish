2c0b0aa8092eac8e74acde8fefb3cf57268825aa
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-11-29 05:31:52 -0500

    add doom to path

--- fish/.config/fish/conf.d/paths.fish
+++ fish/.config/fish/conf.d/paths.fish
@@ -8,6 +8,7 @@ set -l os (uname)
 
 if test "$os" = Linux
     set -U fish_user_paths $HOME/.fzf/bin $fish_user_paths
+    set -U fish_user_paths $HOME/.config/emacs/bin $fish_user_paths
 end
 
 if test "$os" = Darwin
