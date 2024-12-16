17caf75b588e3761a5ffa7277588e5481810d7c5
estifanos <95101485+justEstif@users.noreply.github.com>
2024-12-15 21:32:38 -0500

    feat: add prefered ls abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,9 +1,14 @@
 # abbreviations
 abbr -a v nvim
 abbr -a lz lazygit
+abbr -a ls ls -la
 abbr -a killnode "killall -9 node" # kill all node apps
 
 set -l os (uname)
+
+if test "$os" = Linux
+    abbr -a apt-up "sudo apt update && sudo apt upgrade"
+end
 
 if test "$os" = Darwin
     abbr -a cohesion 'open "$HOME/Documents/frontier/Cohesion Devkit.app"'
