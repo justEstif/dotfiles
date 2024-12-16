758924e8813ce59db02d107a484c9e1cdf817f72
estifanos <95101485+justEstif@users.noreply.github.com>
2024-12-16 08:21:51 -0500

    feat: don't open quickfix on autocmd

--- nvim/.config/nvim/src/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/src/plugins/nvim-lspconfig.lua
@@ -106,5 +106,5 @@ local augroup = vim.api.nvim_create_augroup("strdr4605", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "typescript,typescriptreact",
 	group = augroup,
-	command = "compiler tsc | setlocal makeprg=npx\\ tsc | copen",
+	command = "compiler tsc | setlocal makeprg=npx\\ tsc",
 })
