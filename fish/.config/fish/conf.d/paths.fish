8224116392d4c8e1267ba1b31e8b8735e55f60aa
estifanos <95101485+justEstif@users.noreply.github.com>
2024-12-28 07:59:38 -0500

    feat: remove asdf

--- fish/.config/fish/conf.d/paths.fish
+++ fish/.config/fish/conf.d/paths.fish
@@ -1,14 +1,10 @@
 set -e fish_user_paths
 
 set -U fish_user_paths $HOME/.local/share/nvim/mason/bin $fish_user_paths
-set -U fish_user_paths $HOME/.asdf/shims $fish_user_paths
-set -U fish_user_paths $HOME/.asdf/bin $fish_user_paths
 
 set -l os (uname)
 
 if test "$os" = Linux
-    set -U fish_user_paths $HOME/.fzf/bin $fish_user_paths
-    set -U fish_user_paths $HOME/.config/emacs/bin $fish_user_paths
 end
 
 if test "$os" = Darwin
--- fish/.config/fish/conf.d/variables.fish
+++ fish/.config/fish/conf.d/variables.fish
@@ -1,8 +1,10 @@
 set -gx EDITOR nvim
 set fish_greeting
 
-set -x ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
-set -x ASDF_GOLANG_MOD_VERSION_ENABLED true
-set -x ASDF_NODEJS_LEGACY_FILE_DYNAMIC_STRATEGY true
 set -x RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
-set -x XDG_CONFIG_HOME $HOME/.config
+
+set -l os (uname)
+
+if test "$os" = Darwin
+    set -x XDG_CONFIG_HOME $HOME/.config
+end
