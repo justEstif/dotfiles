53b3e8ea726b420ccfab7977f08f418c2bb94813
estifanos <95101485+justEstif@users.noreply.github.com>
2025-02-10 21:06:48 -0500

    feat: update folding

--- nvim/.config/nvim/after/ftplugin/gitcommit.lua
+++ nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,2 @@
 vim.cmd("setlocal spell wrap")
-vim.cmd("setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1")
+vim.cmd("setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr()")
