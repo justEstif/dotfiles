e60896bb0f22f37fd5ba2e8c0d216f090b1092ce
ebeyene <ebeyene@redventures.com>
2025-02-26 12:14:08 -0500

    fix: mise path on mac and no ruby

--- fish/.config/fish/conf.d/mise.fish
+++ fish/.config/fish/conf.d/mise.fish
@@ -1,1 +1,7 @@
-$HOME/.local/bin/mise activate fish | source
+if test (uname) = "Darwin"
+    # On macOS, use Homebrew path
+    /opt/homebrew/bin/mise activate fish | source
+else
+    # On other systems (Linux, etc.), use the original path
+    $HOME/.local/bin/mise activate fish | source
+end
