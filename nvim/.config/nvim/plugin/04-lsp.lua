746921c8faf44540ce59674df1127a3318899e1a
ebeyene <your_github_username@users.noreply.github.com>
2025-05-09 11:32:42 -0400

    feat: clean up lsp diagnostic

--- nvim/.config/nvim/plugin/04-lsp.lua
+++ nvim/.config/nvim/plugin/04-lsp.lua
@@ -1,58 +1,53 @@
 local add, later = MiniDeps.add, MiniDeps.later
 
 local diagnostic_opts = {
-	-- Define how diagnostic entries should be shown
-	signs = { priority = 9999, severity = { min = "WARN", max = "ERROR" } },
-	underline = { severity = { min = "HINT", max = "ERROR" } },
-	virtual_lines = false,
-	virtual_text = { current_line = true, severity = { min = "ERROR", max = "ERROR" } },
-
-	-- Don't update diagnostics when typing
-	update_in_insert = false,
+  underline = { severity = { min = "HINT", max = "ERROR" } },
+  virtual_text = true,
+  update_in_insert = false,
 }
 
 MiniDeps.later(function()
-	vim.diagnostic.config(diagnostic_opts)
+  vim.diagnostic.config(diagnostic_opts)
 end)
 
 later(function()
-	add("mason-org/mason.nvim")
-	add("mason-org/mason-lspconfig.nvim")
-	add("neovim/nvim-lspconfig")
+  add("mason-org/mason.nvim")
+  add("mason-org/mason-lspconfig.nvim")
+  add("neovim/nvim-lspconfig")
 
-	require("mason").setup()
-	require("mason-lspconfig").setup({
-		ensure_installed = { "lua_ls", "ts_ls", "cssls" },
-	})
+  require("mason").setup()
+  require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "ts_ls", "cssls" },
+  })
 end)
 
 later(function()
-	add("stevearc/conform.nvim")
-	local conform = require("conform")
-	conform.setup({
-		formatters_by_ft = {
-			css = { "prettier" },
-			fish = { "fish_indent" },
-			html = { "prettier" },
-			javascript = { "prettier" },
-			json = { "prettier" },
-			lua = { "stylua" },
-			markdown = { "prettier" },
-			toml = { "taplo" },
-			typescript = { "prettier" },
-			typescriptreact = { "prettier" },
-		},
-	})
+  add("stevearc/conform.nvim")
+  local conform = require("conform")
+  conform.setup({
+    formatters_by_ft = {
+      css = { "prettier" },
+      fish = { "fish_indent" },
+      html = { "prettier" },
+      javascript = { "prettier" },
+      json = { "prettier" },
+      lua = { "stylua" },
+      markdown = { "prettier" },
+      toml = { "taplo" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+    },
+  })
 end)
 
 later(function()
-	add("mfussenegger/nvim-lint")
-	local lint = require("lint")
-	lint.linters_by_ft = {
-		css = { "eslint" },
-		javascript = { "eslint" },
-		json = { "eslint" },
-		typescript = { "eslint" },
-		typescriptreact = { "eslint" },
-	}
+  add("mfussenegger/nvim-lint")
+  local lint = require("lint")
+  lint.linters_by_ft = {
+    css = { "eslint" },
+    javascript = { "eslint" },
+    json = { "eslint" },
+    typescript = { "eslint" },
+    typescriptreact = { "eslint" },
+  }
 end)
