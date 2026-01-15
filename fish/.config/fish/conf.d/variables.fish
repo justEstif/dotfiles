9fd0497fa6651a5190571575b9b0cd4d373161e6
e-beyene <171644405+e-beyene@users.noreply.github.com>
2026-01-15 13:29:29 -0500

    feat: enable opencode experiemental plan mode

--- fish/.config/fish/conf.d/variables.fish
+++ fish/.config/fish/conf.d/variables.fish
@@ -4,6 +4,7 @@ set fish_greeting
 set -x RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
 set -x FZF_DEFAULT_OPTS "--preview 'cat {}'"
 set -x OPENCODE_EXPERIMENTAL_LSP_TOOL true
+set -x OPENCODE_EXPERIMENTAL_PLAN_MODE true
 
 set -l os (uname)
 
