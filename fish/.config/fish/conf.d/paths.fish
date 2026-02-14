ad85bba657f0275df74b8555bd5a64297688f30f
estifanos <95101485+justEstif@users.noreply.github.com>
2026-02-14 16:04:17 -0500

    feat: add paths

--- fish/.config/fish/conf.d/paths.fish
+++ fish/.config/fish/conf.d/paths.fish
@@ -1,13 +1,15 @@
 set -e fish_user_paths
 
 set -U fish_user_paths $HOME/.local/share/nvim/mason/bin $fish_user_paths
+set -U fish_user_paths "$HOME/.local/bin" $fish_user_paths
 
 set -l os (uname)
 
 if test "$os" = Linux
+    # Doom Emacs
+    set -gx PATH $HOME/.config/emacs/bin $PATH
 end
 
 if test "$os" = Darwin
     set -U fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $fish_user_paths
-    set -U fish_user_paths "$HOME/.local/bin" $fish_user_paths
 end
