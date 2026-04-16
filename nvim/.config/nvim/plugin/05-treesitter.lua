8ec298982ea41875fec4c034371b4d8f1ea57d5b
estifanos <95101485+justEstif@users.noreply.github.com>
2026-04-16 17:33:14 -0400

    feat: treesitter is built-in now

--- nvim/.config/nvim/plugin/05-treesitter.lua
+++ nvim/.config/nvim/plugin/05-treesitter.lua
@@ -1,54 +1,11 @@
-local add, later = MiniDeps.add, MiniDeps.later
-
-local ensure_installed = {
-	"comment",
-	"css",
-	"diff",
-	"fish",
-	"gitcommit",
-	"gitignore",
-	"html",
-	"javascript",
-	"json",
-	"lua",
-	"markdown",
-	"markdown_inline",
-	"prisma",
-	"python",
-	"sql",
-	"svelte",
-	"tsx",
-	"typescript",
-}
-
-local ts_spec = {
-	source = "nvim-treesitter/nvim-treesitter",
-	checkout = "master",
-	hooks = {
-		post_checkout = function()
-			vim.cmd("TSUpdate")
-		end,
-	},
-}
+local later = MiniDeps.later
 
 later(function()
-	add({
-		source = "nvim-treesitter/nvim-treesitter-textobjects",
-		depends = { ts_spec },
-	})
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = ensure_installed,
-		auto_install = true,
-		highlight = { enable = true },
-		incremental_selection = { enable = false },
-		textobjects = { enable = false },
-		indent = { enable = false },
+	vim.api.nvim_create_autocmd("FileType", {
+		callback = function(ev)
+			pcall(vim.treesitter.start, ev.buf)
+		end,
 	})
 
-	-- Disable injections in 'lua' language. In Neovim<0.9 it is
-	-- `vim.treesitter.query.set_query()`; in Neovim>=0.9 it is
-	-- `vim.treesitter.query.set()`.
-	local ts_query = require("vim.treesitter.query")
-	local ts_query_set = ts_query.set or ts_query.set_query
-	ts_query_set("lua", "injections", "")
+	vim.treesitter.query.set("lua", "injections", "")
 end)
