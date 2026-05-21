856519a88edb054b2404735ea096796f3b74b49c
estifanos <95101485+justEstif@users.noreply.github.com>
2026-05-21 08:55:37 -0400

    chore

--- fish/.config/fish/functions/pi.fish
+++
@@ -1,3 +0,0 @@
-function pi --wraps pi --description 'pi coding agent with bare nvim as external editor'
-    VISUAL="nvim --clean -u $HOME/.config/nvim/pi-clean.vim" mise x node@lts -- pi $argv
-end
