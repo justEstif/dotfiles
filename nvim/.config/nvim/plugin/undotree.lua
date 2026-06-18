4d49faf3aad25debb23a412b9fcf8f165dcf2b61
estifanos <95101485+justEstif@users.noreply.github.com>
2026-06-18 09:09:33 -0400

    chore: clean up

--- nvim/.config/nvim/plugin/undotree.lua
+++
@@ -1,6 +0,0 @@
-vim.keymap.set("n", "<leader>u", function()
-  -- Load the built-in undotree plugin
-  vim.cmd("packadd nvim.undotree")
-  -- Toggle or open it
-  vim.cmd("Undotree")
-end, { desc = "Toggle UndoTree" })
