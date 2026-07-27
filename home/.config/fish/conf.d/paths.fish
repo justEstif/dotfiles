4db1e17264b9c5385ca4dedfdeed0793f897a7e0
e-beyene <171644405+e-beyene@users.noreply.github.com>
2026-07-27 08:51:58 -0400

    fix(fish): add Homebrew to macOS PATH

--- home/.config/fish/conf.d/paths.fish
+++ home/.config/fish/conf.d/paths.fish
@@ -2,3 +2,7 @@ set -e fish_user_paths
 
 set -U fish_user_paths $HOME/.local/share/nvim/mason/bin $fish_user_paths
 set -U fish_user_paths "$HOME/.local/bin" $fish_user_paths
+
+if test (uname) = Darwin
+    fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
+end
