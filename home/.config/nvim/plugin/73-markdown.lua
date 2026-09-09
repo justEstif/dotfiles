bd086e4655886315f475e41cb19b2a45a382f830
justEstif <95101485+justEstif@users.noreply.github.com>
2026-09-09 08:59:07 -0400

    nvim: tone render-markdown down to a minimal look

--- home/.config/nvim/plugin/73-markdown.lua
+++ home/.config/nvim/plugin/73-markdown.lua
@@ -1,4 +1,4 @@
--- Nicer markdown viewing: render-markdown.nvim + <mark> highlighting
+-- Nicer markdown viewing: render-markdown.nvim (minimal) + <mark> highlighting
 local add = vim.pack.add
 local later = Config.later
 
@@ -6,12 +6,47 @@ later(function()
 	add({ "https://github.com/MeanderingProgrammer/render-markdown.nvim" })
 
 	require("render-markdown").setup({
-		-- Keep syntax visible while typing on a line, render when idle
 		render_modes = { "n", "c", "t" },
 		anti_conceal = { enabled = true },
+
+		-- Minimal: quiet everything down
 		heading = {
-			icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+			icons = {}, -- no 󰲡 icons, just styled text
+			signs = { enabled = false },
+			position = "inline",
+			width = "full",
+			backgrounds = {}, -- no colored heading background
+			foregrounds = {
+				"MarkdownH1",
+				"MarkdownH2",
+				"MarkdownH3",
+				"MarkdownH4",
+				"MarkdownH5",
+				"MarkdownH6",
+			},
 		},
+		dash = { icon = "─" }, -- thin thematic break
+		bullet = {
+			icons = {}, -- keep plain -, 1. markers
+			right_pad = 1,
+		},
+		checkbox = {
+			position = "inline",
+			unchecked = { icon = "◯ " },
+			checked = { icon = "◉ " },
+			custom = { todo = { raw = "[-]", rendered = "◐ " } },
+		},
+		code = {
+			sign = false,
+			style = "language", -- just the language label, no background blocks
+			position = "left",
+		},
+		table = { style = "none" }, -- keep raw pipes
+		link = {
+			image = "󰇞 ", -- tiny image marker
+		},
+		quote = { icon = "" }, -- no big quote bar replacement
+		sign = { enabled = false },
 	})
 
 	-- <mark>...</mark> and ==text==: paint the inner text like browser <mark>
