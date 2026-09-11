7e34050c2c1626ad3e4fba473d318b29087103fd
ebeyene <171644405+e-beyene@users.noreply.github.com>
2026-09-11 10:50:57 -0400

    fish: move file pickers to television

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -2,7 +2,7 @@ abbr -a v nvim
 abbr -a lz lazygit
 abbr -a l ls -1hA # ls column view, hidden files
 abbr -a killnode "killall -9 node" # kill all node apps
-abbr -a fzfp 'find "$PWD" | fzf' # full file path - fzf
+abbr -a fzfp 'tv files "$PWD"' # pick a full file path with Television
 
 set -l os (uname)
 
